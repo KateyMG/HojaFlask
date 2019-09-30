a3c3abc1d4146ce9171c66f76bc90589daf5794e
KateyMG <46852046+KateyMG@users.noreply.github.com>
2019-09-30 15:03:06 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+#FLASK
+from flask import Flask, jsonify, render_template, request
+import os, optparse
+import yaml 
+
+
+app = Flask(__name__)
+
+developer = os.getenv("DEVELOPER", "Katherine Mazariegos")
+environment=os.getenv("ENVIRONMENT","development")
+config=yaml.load(open('info.yml')) 
+print(config['Nombre'])
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
+
+@app.route("/info")
+def api_students():
+    return render_template("info.html", config=config)
+   
+@app.route("/formacion")
+def formacion():
+    return render_template("formacion.html")
+
+
+@app.route("/experiencia")
+def experiencia():
+    return render_template("experiencia.html")
+
+@app.route("/")
+def home():
+    foo="bar"
+    return render_template("home.html", mivariable=foo ,developer=developer)
+
+if __name__ == "__main__":
+    debug=False
+    if environment == "development" or environment == "local":
+        debug=True
+    app.run(host="0.0.0.0",debug=debug)
